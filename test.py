3b0112ba07797efbf83a201c41ddcb509737f476
Leon Blum <you@example.com>
2021-08-28 17:53:52 +0200

    test

--- test.py
+++ test.py
@@ -8,26 +8,27 @@ from fastbook import *
 from fastai.vision.widgets import *
 from IPython.display import display
 
-try:
-    learn_inf = load_learner('bird_mmodel.pkl', cpu=True)
-except:
-    raise Error
-btn_upload = widgets.FileUpload()
-out_pl = widgets.Output()
-lbl_pred = widgets.Label()
 
-def on_click(change):
-    img = PILImage.create(btn_upload.data[-1])
-    out_pl.clear_output()
-    with out_pl: display(img.to_thumb(128,128))
-    pred,pred_idx,probs = learn_inf.predict(img)
-    lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
+# try:
+#     learn_inf = load_learner('bird_mmodel.pkl', cpu=True)
+# except:
+#     raise Error
+# btn_upload = widgets.FileUpload()
+# out_pl = widgets.Output()
+# lbl_pred = widgets.Label()
+
+# def on_click(change):
+#     img = PILImage.create(btn_upload.data[-1])
+#     out_pl.clear_output()
+#     with out_pl: display(img.to_thumb(128,128))
+#     pred,pred_idx,probs = learn_inf.predict(img)
+#     lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
     
-btn_upload.observe(on_click, names=['data'])
+# btn_upload.observe(on_click, names=['data'])
 
-display(VBox(
-    [widgets.Label('Select your bird!'), 
-    btn_upload, out_pl, lbl_pred]
-    ))
+# display(VBox(
+#     [widgets.Label('Select your bird!'), 
+#     btn_upload, out_pl, lbl_pred]
+#     ))
 
 
